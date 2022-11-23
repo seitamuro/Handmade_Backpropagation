cdb61d34a604009c3e4368a69a711b8bc96a35e3
seitamuro <seitamuro0210@gmail.com>
2022-11-23 22:40:19 +0900

    Affine Layer

---
+++ layers.py
@@ -0,0 +1,13 @@
+import numpy as np
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+
+    def forward(self, x):
+        return np.dot(x, self.W) + self.b
+
+    def backward(self, dout):
+        dW = np.dot(dout, self.W.T)
+        db = dout
+        return dW, db
