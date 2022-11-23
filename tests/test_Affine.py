86b1a2c147a08e91065a144c5b2392583abaa246
seitamuro <seitamuro0210@gmail.com>
2022-11-23 23:05:17 +0900

    [test] Affine forward

--- tests/test_Affine.py
+++ tests/test_Affine.py
@@ -4,5 +4,15 @@ import numpy as np
 
 class TestAffineLayer(unittest.TestCase):
     def test_init(self):
-        a = Affine(np.random.random(1, 1), np.random.random(1))
+        a = Affine(np.random.random((1, 1)), np.random.random(1))
+
+    def test_forward(self):
+        W = np.array([[1, 2, 3]])
+        b = np.array([1, 2, 3])
+        x = np.array([1])
+        y = np.array([2, 4, 6])
+        layer = Affine(W, b)
+        self.assertTrue((y == layer.forward(x)).all())
+
+
 
