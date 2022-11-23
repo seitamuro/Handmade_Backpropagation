abf060642a4243e48d62d1d851ef5d8f4ed3764f
seitamuro <seitamuro0210@gmail.com>
2022-11-23 23:08:42 +0900

    [feat] add dW db

--- layers.py
+++ layers.py
@@ -4,11 +4,13 @@ class Affine:
     def __init__(self, W, b):
         self.W = W
         self.b = b
+        self.dW = None
+        self.db = None
 
     def forward(self, x):
         return np.dot(x, self.W) + self.b
 
     def backward(self, dout):
-        dW = np.dot(dout, self.W.T)
-        db = dout
-        return dW, db
+        self.dW = np.dot(dout, self.W.T)
+        self.db = dout
+        return self.dW, self.db
