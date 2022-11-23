deae247a28ac5ebf19437c8424d4e98af304ba08
seitamuro <seitamuro0210@gmail.com>
2022-11-24 00:24:36 +0900

    [test] ReLU backward

--- tests/test_ReLU.py
+++ tests/test_ReLU.py
@@ -26,3 +26,30 @@ class TestReLULayer(unittest.TestCase):
         t = np.array([[0, 0, 0]])
         y = layer.forward(x)
         self.assertTrue((y == t).all())
+
+    def test_backward_x_gt_0(self):
+        layer = ReLU()
+        x = np.array([[1, 2, 3]])
+        dout = np.array([[5, 5, 5]])
+        t = np.array([[5, 5, 5]])
+        layer.forward(x)
+        y = layer.backward(dout)
+        self.assertTrue((y == t).all())
+
+    def test_backward_x_eq_0(self):
+        layer = ReLU()
+        x = np.array([[0, 0, 0]])
+        dout = np.array([[5, 5, 5]])
+        t = np.array([[0, 0, 0]])
+        layer.forward(x)
+        y = layer.backward(dout)
+        self.assertTrue((y == t).all())
+
+    def test_backward_x_lt_0(self):
+        layer = ReLU()
+        x = np.array([[-1, -2, -3]])
+        dout = np.array([[5, 5, 5]])
+        t = np.array([[0, 0, 0]])
+        layer.forward(x)
+        y = layer.backward(dout)
+        self.assertTrue((y == t).all())
