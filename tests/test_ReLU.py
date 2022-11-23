a871968e56822738cf389aee501702b0fe72e82f
seitamuro <seitamuro0210@gmail.com>
2022-11-24 00:21:43 +0900

    [test] ReLU forward

--- tests/test_ReLU.py
+++ tests/test_ReLU.py
@@ -5,3 +5,24 @@ from layers import ReLU
 class TestReLULayer(unittest.TestCase):
     def test_init(self):
         layer = ReLU()
+
+    def test_forward_x_gt_0(self):
+        layer = ReLU()
+        x = np.array([[1, 2, 3]])
+        t = np.array([[1, 2, 3]])
+        y = layer.forward(x)
+        self.assertTrue((y == t).all())
+
+    def test_forward_x_eq_0(self):
+        layer = ReLU()
+        x = np.array([[0, 0, 0]])
+        t = np.array([[0, 0, 0]])
+        y = layer.forward(x)
+        self.assertTrue((y == t).all())
+
+    def test_forward_x_lt_0(self):
+        layer = ReLU()
+        x = np.array([[-1, -2, -3]])
+        t = np.array([[0, 0, 0]])
+        y = layer.forward(x)
+        self.assertTrue((y == t).all())
